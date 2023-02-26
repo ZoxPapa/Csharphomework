5556c61ab5f9d2dc4e3ebf42a7ef4b1d11474f86
Zakhar <zox_13@mail.ru>
2023-02-26 16:12:44 +0300

    First Task complete

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input first number: ");
+int first_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int second_num = Convert.ToInt32(Console.ReadLine());
+if (first_num > second_num)
+    Console.Write("{0} is bigger than {1}.", first_num, second_num);
+else if (first_num < second_num)
+    Console.Write("{1} is bigger than {0}.", first_num, second_num);
+else Console.Write("Numbers are equal.");
