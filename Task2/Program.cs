b12296faf70f9f750efbce2342d4be6a26faf647
Zakhar <zox_13@mail.ru>
2023-02-26 16:48:21 +0300

    Task2 complete

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input first number: ");
+int first_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int second_num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number: ");
+int third_num = Convert.ToInt32(Console.ReadLine());
+int max = first_num;
+if (first_num < second_num)
+    max = second_num;
+if (third_num > max)
+    max = third_num;
+
+Console.Write($"Max number is: {max}");
