d8a44d3a6a18e26ec955e388311bcf8bc45afedb
Zakhar <zox_13@mail.ru>
2023-02-26 17:29:23 +0300

    Task4 complete

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+if (number < 2)
+    Console.Write("Number is incorrect");
+else 
+    while(i <= number)
+    {
+        Console.Write(i + "/");
+        i+=2;
+    }
