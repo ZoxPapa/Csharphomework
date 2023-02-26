b77876f45d4def118400f73f8b89ebab58ca814c
Zakhar <zox_13@mail.ru>
2023-02-26 17:16:51 +0300

    Task3 complete

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number == 0)
+    Console.Write("Inputed number is 0, please input correct number");
+else if(number%2 == 0)
+    Console.Write($"{number} is even number");
+else 
+    Console.Write($"{number} is odd number");
