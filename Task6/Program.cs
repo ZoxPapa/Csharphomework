3ca213f1b4b320b2c3a8a871ff3f4a4e535ff36b
Zakhar <zox_13@mail.ru>
2023-02-27 19:56:29 +0300

    Task6 complete

---
+++ Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Please input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num >99 || num <-99)
+    Console.Write($"Third digit is {(num/100)%10}");
+else 
+    Console.Write("Third digit is absent");
