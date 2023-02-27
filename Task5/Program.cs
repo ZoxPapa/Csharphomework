f0810cbcc3d2493f4826f97124dff5def58789f0
Zakhar <zox_13@mail.ru>
2023-02-27 19:26:10 +0300

    Task5 complete

---
+++ Task5/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Please input three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num > 99 && num <1000)
+    Console.Write($"Result is - {(num/10)%10}");
+else
+    Console.Write("Error. You input wrong number.");
