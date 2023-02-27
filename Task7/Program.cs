86f3c1c021d940c9b6e64cdf694a60c663e4d8ed
Zakhar <zox_13@mail.ru>
2023-02-27 20:22:13 +0300

    Task7 complete

---
+++ Task7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Please input number of the day of the week (1 to 7): ");
+int day = Convert.ToInt32(Console.ReadLine());
+if(day >0 && day < 6)
+    Console.Write("Опять работа?");
+else if(day >5 && day <8)
+    Console.Write("Выходной. Нужно построить зиккурат");
+else
+    Console.Write("Error. There are only 7 days in a week =)");
