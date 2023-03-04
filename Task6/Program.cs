ada289565229471ca7034f5b6d1e732b9b071eea
Zakhar <zox_13@mail.ru>
2023-03-04 09:46:20 +0300

    Task8 complete

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -1,6 +1,16 @@
 ﻿Console.Write("Please input number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-if(num >99 || num <-99)
-    Console.Write($"Third digit is {(num/100)%10}");
-else 
+
+//Для вывода третьей цифры справа:
+// int num = Convert.ToInt32(Console.ReadLine());
+// if(num >99 || num <-99)
+//     Console.Write($"Third digit is {(num/100)%10}");
+// else 
+//     Console.Write("Third digit is absent");
+
+//Для вывода третьей цифры слева:
+string num = Console.ReadLine();
+if(num.Length>2)
+    Console.Write($"Third number is: {num[2]}");
+else
     Console.Write("Third digit is absent");
+    
---
+++ Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Input legth of number: ");
+int len = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Input {len} digits number: ");
+string digits = Console.ReadLine();
+int number = Convert.ToInt32(digits); //Проверка, что введено число
+bool result = true;
+
+if(digits.Length != len) // Проверка, что число нужной длины
+    Console.WriteLine($"Error. You need to input {len} digits number. ");
+else
+{
+    for (int i = 0 ; i < (len-1-i); i++)
+    {
+        if(digits[i] == digits[(len-1-i)]) //Проверка на полиндромность
+            result = true;
+        else result = false;
+            break;
+    }
+    if(result == true)
+        Console.Write("It's polyndrome ");
+    else Console.Write("It's not polyndrome ");
+}
