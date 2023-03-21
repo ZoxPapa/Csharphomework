9172ffa4fc8da404b7c8339cff6cabde1aeac701
Zakhar <zox_13@mail.ru>
2023-03-21 11:40:14 +0300

    Homework 20.3.23 complete

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -49,16 +49,16 @@ void FindMinRow(int[,] array)
 {
     int[] array1d = new int[array.GetLength(0)];
     int minRowNumber = 0;
-    for (int i = 0, minSumRow = 0;i < array.GetLength(0); i++)
+    for (int i = 0, minSumRow = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
             array1d[i] += array[i, j];
         if (i == 0) minSumRow = array1d[0];
         if (array1d[i] < minSumRow)
-            {
-                minSumRow = array1d[i];
-                minRowNumber = i;
-            }
+        {
+            minSumRow = array1d[i];
+            minRowNumber = i;
+        }
     }
     Console.WriteLine($"Row with minimal sum of numbers is {minRowNumber + 1}");
 }
@@ -68,23 +68,23 @@ void FindMinRow(int[,] array)
 
 //Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
-int[,] Multiple2dArray (int [,] firstArray, int[,]secondArray)
-{   
-    if (firstArray.GetLength(1)!= secondArray.GetLength(0))
+int[,] Multiple2dArray(int[,] firstArray, int[,] secondArray)
+{
+    if (firstArray.GetLength(1) != secondArray.GetLength(0))
     {
         Console.WriteLine("This matrices can't be multiplied. Change qty columns and rows. ");
-        int [,] badresult = new int[1,50];
+        int[,] badresult = new int[1, 50];
         return badresult;
     }
     else
     {
-        int [,] result = new int [firstArray.GetLength(0), secondArray.GetLength(1)];
+        int[,] result = new int[firstArray.GetLength(0), secondArray.GetLength(1)];
         for (int i = 0; i < firstArray.GetLength(0); i++)
         {
             for (int k = 0; k < secondArray.GetLength(1); k++)
             {
                 for (int j = 0; j < secondArray.GetLength(0); j++)
-                    result[i,k] += firstArray[i,j]*secondArray[j,k];
+                    result[i, k] += firstArray[i, j] * secondArray[j, k];
             }
         }
         return result;
@@ -103,40 +103,71 @@ int[,] Multiple2dArray (int [,] firstArray, int[,]secondArray)
 
 // Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 // Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-// Массив размером 2 x 2 x 2
-// 66(0,0,0) 25(0,1,0)
-// 34(1,0,0) 41(1,1,0)
-// 27(0,0,1) 90(0,1,1)
-// 26(1,0,1) 55(1,1,1)
 
-int [,,] CreateOutput3DArray (int m, int n, int z)
+int[,,] CreateOutput3DArray(int m, int n, int z)
 {
-    if(m*n*z >90)
+    if (m * n * z > 90)
     {
         Console.WriteLine("Error.Imposible to create 2digits array with this parametres.");
-        int [,,] badresult = new int[1,1,50];
+        int[,,] badresult = new int[1, 1, 50];
         return badresult;
     }
     int[,,] array = new int[m, n, z];
-    int difference =1;
+    int difference = 1;
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                array[i, j, k] = 9+difference;
+                array[i, j, k] = 9 + difference;
                 Console.Write(array[i, j, k] + $"({i},{j},{k}) ");
-                difference +=1;
+                difference += 1;
             }
             Console.WriteLine();
         }
     }
     return array;
 }
-CreateOutput3DArray(3,3,10);
+// CreateOutput3DArray(3,3,10);
 
 //-------------------------------------------------------------------------------------------------
 
-// Напишите программу, которая заполнит спирально массив 4 на 4.
-// Например, на выходе получается вот такой массив:
+// Напишите программу, которая заполнит спирально массив n x m.
+
+int[,] FillSpiral(int[,] array)
+{
+    int i = 0, j = 0, count = 1;
+    array[i, j] = count;
+    while (count < array.GetLength(0) * array.GetLength(1))
+    {
+        while (j + 1 < array.GetLength(1) && array[i, j + 1] == 0) //Filling right
+        {
+            j++;
+            count++;
+            array[i, j] = count;
+        }
+        while (i + 1 < array.GetLength(0) && array[i + 1, j] == 0) //Filling down
+        {
+            i++;
+            count++;
+            array[i, j] = count;
+        }
+        while (j - 1 >= 0 && array[i, j - 1] == 0) // Filling left
+        {
+            j -= 1;
+            count++;
+            array[i, j] = count;
+        }
+        while (array[i-1, j] == 0) // filling up
+        {
+            i -= 1;
+            count++;
+            array[i, j] = count;
+        }
+    }
+    return array;
+
+}
+// int[,] array = new int[7, 7];
+// Output2DArray(FillSpiral(array));
---
+++ Task15/Program.cs
@@ -0,0 +1,25 @@
+﻿//------------------------------------------------------------------------------------------------
+
+//Вывести первые N строк треугольника Паскаля. 
+//Сделать вывод в виде равнобедренного треугольника
+
+void PascalTriangle(int qtyrows)
+{
+    int horizontalposition = 50;
+    int verticalposition = 1;
+    int[] firstRow = { 0, 1, 0 };
+    Console.SetCursorPosition(horizontalposition, verticalposition);
+    Console.WriteLine(String.Join("", firstRow));
+    for (int j = 0; j < qtyrows - 1; j++)
+    {
+        Console.SetCursorPosition(horizontalposition - 1 - j, verticalposition + 1 + j);
+        int[] secondRow = new int[firstRow.Length + 2];
+        secondRow[1] = 1;
+        secondRow[secondRow.Length - 2] = 1;
+        for (int i = 1; i < firstRow.Length - 2; i++)
+            secondRow[i + 2] = firstRow[i] + firstRow[i + 2];
+        Console.WriteLine(String.Join("", secondRow));
+        firstRow = secondRow;
+    }
+}
+PascalTriangle(8);
